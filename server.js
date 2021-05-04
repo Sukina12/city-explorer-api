d8c4bb3b3fc643a921df21ae4257080c28a58442
Sukina AbuHammad <sukina_89@yahoo.com>
2021-05-05 02:33:35 +0300

    edit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ let PORT = process.env.PORT
 const app = express()
 app.use(cors())
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send(data)
 })
 app.get('/weather', function (req, res) {
   const arrOfData = weather.data.map(data => new Weather(data));
